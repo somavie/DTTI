ea7c2f09ae67be2f39118bcc782fd5a84a435c63
BASS@@ <106202482+somavie@users.noreply.github.com>
2024-10-23 13:45:09 +0100

    v2

--- frontend/components/relatorios/relatorio.tsx
+++ frontend/components/relatorios/relatorio.tsx
@@ -77,10 +77,6 @@ export const Relatorios = () => {
     [fetchRelatorios]
   );
 
-  const handleFilteredData = (filteredData: RelatorioType[]) => {
-    setRelatorios(filteredData); // Atualiza a tabela com os dados filtrados
-  };
-
   return (
     <>
       <div className="my-2 px-4 lg:px-6 max-w-[95rem] mx-auto w-full flex flex-col gap-4">
