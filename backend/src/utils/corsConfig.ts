5bcfd8c0043fb555953840b9d7ed4de5177056af
BASS@@ <106202482+somavie@users.noreply.github.com>
2025-02-26 11:30:35 +0100

    Update corsConfig.ts

--- backend/src/utils/corsConfig.ts
+++ backend/src/utils/corsConfig.ts
@@ -1,5 +1,5 @@
 export const corsOptions = {
-  origin: "*", // URL do frontend
+  origin: "http://192.168.104.235:3000", // URL do frontend
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // Permite envio de cookies e cabeçalhos de autorização
